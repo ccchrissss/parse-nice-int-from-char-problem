ea26b573a48b21c6e4ccd283834f740fe8b4aa50
Chris Nelson <chrisnelson7791@gmail.com>
2024-03-27 14:29:30 -0700

    First commit

---
+++ main.js
@@ -0,0 +1,15 @@
+function getAge(inputString){
+  
+  return Number(inputString[0])
+
+}
+
+console.log(getAge('7 years old'))
+  
+  // params: inputString  // a phrase where the first character in the string is the age as a number
+  // return: age  // an integer
+  // e.g. getAge('7 years old')
+  // results in 7
+  // 
+
+  // return Number(inputString[0])
